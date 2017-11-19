5595cbd4b5dcfae768fde8371576c4de9b489623
Jeanderson Candido <jeandersonbc@gmail.com>
2017-11-19 02:58:27 -0300

    Added user input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+    println!("Please, input your guess.");
+
+    // "let" statements allow to create variables...
+    // In Rust, variables are IMMUTABLE by default! To make the variable
+    // multable, you have to add the modifier "mut", as well.
+    // Notice that, if you try to change the value of an immutable variable,
+    // the Rust compiler will complain about it.
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }

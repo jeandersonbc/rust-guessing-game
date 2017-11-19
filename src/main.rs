09c4ce59d92e5c51da83319d01267729c7e7f954
Jeanderson Candido <jeandersonbc@gmail.com>
2017-11-19 11:12:50 -0300

    Added notes about references and error-handling

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,17 @@ fn main() {
     // the Rust compiler will complain about it.
     let mut guess = String::new();
 
+    // The "&" operator refers to the memory address of the variable "guess".
+    // Again, by default, references are immutable. In this case, we need to
+    // make the reference mutable in other get the input.
+    //
+    // Another curious thing here is the ".expect" function. The function
+    // "read_line" returns a "Result" which indicates if the function was
+    // successfully executed. The Result type is responsible for encapsulating
+    // error-handling and the Rust compiler will emit a warning whenever you
+    // forget to use the return value of a function that you are supposed to be
+    // using. In this case, if something bad occurs, the execution halts and
+    // the informed message will be displayed.
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
